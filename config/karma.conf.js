5ca91f6a079d289895828367aaf651d366cc9e0d
Francesco Scali <francesco.scali@gmail.com>
2013-12-26 14:40:42 +0100

    migrated to express framework and changed project structure

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -3,17 +3,17 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
-      'app/lib/angular/angular.js',
-      'app/lib/angular/angular-*.js',
+      'app/public/lib/angular/angular.js',
+      'app/public/lib/angular/angular-*.js',
       'test/lib/angular/angular-mocks.js',
-      'app/js/**/*.js',
+      'app/public/js/**/*.js',
       'test/unit/**/*.js'
     ],
 
     exclude : [
-      'app/lib/angular/angular-loader.js',
-      'app/lib/angular/*.min.js',
-      'app/lib/angular/angular-scenario.js'
+      'app/public/lib/angular/angular-loader.js',
+      'app/public/lib/angular/*.min.js',
+      'app/public/lib/angular/angular-scenario.js'
     ],
 
     autoWatch : true,
